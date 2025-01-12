edfed218a3a7a93243ba35c4571ac814a3b823df
Dragoft <16841977@qq.com>
2025-01-12 14:15:47 +0800

    UPDATE

--- functions/admin.api.js
+++ functions/admin.api.js
@@ -7,11 +7,9 @@
 
 	// 登录
 	if (m == "0" && body.key) {
-		var key = await context.env.MetaDB.prepare('SELECT * from root where data="adminKey"').first()
-		var key = key.content
+		var key = await context.env.MetaDB.prepare('SELECT * from root where data="adminKey"').first().content
 		r.success = true
 		r.msg = null
-
 		if (body.key == key) {
 			r.results.push({"login": 1})
 		} else {
@@ -22,11 +20,18 @@
 	// 修改密码
 	if (m == "1" && body.old && body.new) {
 		var key = await context.env.MetaDB.prepare('SELECT * from root where data="adminKey"').first().content
-		var key = key.content
-
 		if (body.old == key) {
 			r = await context.env.MetaDB.prepare('UPDATE root set content=? where data="adminKey"').bind(body.new).all()
-			r.msg = "key changed"
+			r.msg = "change key"
+		}
+	}
+
+	// 执行 SQL 命令
+	if (m == "2" && body.key && body.sql) {
+		var key = await context.env.MetaDB.prepare('SELECT * from root where data="adminKey"').first().content
+		if (body.key == key) {
+			r = await context.env.MetaDB.prepare(sql).all()
+			r.msg = null
 		}
 	}
 
--- js/admin.js
+++ js/admin.js
@@ -30,16 +30,21 @@ const env = {
 
 env.f.analysis = function(str) {
 	// 解析命令
-	var cmd = str.match(/"[^"]+"|\S+/g) || []
+	var cmd = (str.match(/"[^"]+"|\S+/g) || []).map(part => part.replace(/^"|"$/g, ''))
 	env.f.write('<cmd>' + str + '</cmd>')
 
 	if (cmd[0] == 'help') {
 		env.f.write(`---------------------- [ 帮助 ] ----------------------
 help							查看帮助信息
 cls							清除控制台
+
 login "密码"					登录
 login "旧密码" "新密码"			修改密码
-login out						退出登录`)
+login out						退出登录
+
+select name from sqlite_schema where type='table' and name != '_cf_KV' ORDER BY name		查询所有表名
+select * from 表名																	查询指定表的全部数据
+select * from 表名 where 列名='行名'													查询指定表指定行的数据`)
 		return
 	}
 	if (cmd[0] == 'cls') {
@@ -55,7 +60,7 @@ login out						退出登录`)
 				return
 			}
 
-			env.f.write('fetch:<info>\n	- api: "https://' + env.data.domain + '/admin.api"\n	- key: ' + cmd[1] + '</info>')
+			env.f.write('fetch:<info>\n	- api: "https://' + env.data.domain + '/admin.api"\n	- key: "' + cmd[1] + '"</info>')
 			fetch('https://' + env.data.domain + '/admin.api', {
 				method: "POST",
 				headers: {
@@ -72,10 +77,10 @@ login out						退出登录`)
 			})
 			.then(json => {
 				if (json.results[0].login) {
-					env.f.write('<su>[succeeded] </su>your key is correct, you have successfully logged in now')
+					env.f.write('<su>[SUCCEED] </su>your key is correct, you have successfully logged in now')
 					env.data.key = cmd[1]
 				} else {
-					env.f.write('<err>[failed] </err>your key is incorrect, please try again')
+					env.f.write('<err>[FAILED] </err>your key is incorrect, please try again')
 				}
 				return
 			})
@@ -83,7 +88,7 @@ login out						退出登录`)
 		}
 
 		if (cmd.length == 3 && env.data.key) {
-			env.f.write('fetch:<info>\n	- api: "https://' + env.data.domain + '/admin.api"\n	- original_key: ' + cmd[1] + '\n	- new_key: ' + cmd[2] + '</info>')
+			env.f.write('fetch:<info>\n	- api: "https://' + env.data.domain + '/admin.api"\n	- original_key: "' + cmd[1] + '"\n	- new_key: "' + cmd[2] + '"</info>')
 			if (cmd[1] == env.data.key) {
 				fetch('https://' + env.data.domain + '/admin.api', {
 					method: "POST",
@@ -101,28 +106,50 @@ login out						退出登录`)
 					}
 				})
 				.then(json => {
-					env.f.write('key changed: "' + cmd[1] + '" => "' + cmd[2] + '"')
+					env.f.write('<su>[SUCCEED] </su> key changed: "' + cmd[1] + '" => "' + cmd[2] + '"')
 					env.data.key = cmd[2]
 					return
 				})
 				.catch(err => {env.f.write(('<err>' + err + '</err>').toLowerCase())})
 			} else {
-				env.f.write('<err>[failed] </err>your original key is incorrect, please try again')
+				env.f.write('<err>[FAILED] </err>your original key is incorrect, please try again')
 			}
 		}
 	}
 
-
+	if (['update', 'select', 'create', 'drop', 'alter', 'insert', 'delete'].includes(cmd[0])) {
+		env.f.write('fetch:<info>\n	- api: "https://' + env.data.domain + '/admin.api</info>')
+		fetch('https://' + env.data.domain + '/admin.api', {
+			method: "POST",
+			headers: {
+				"Token": 2,
+			},
+			body: JSON.stringify({
+				"sql": str,
+			})
+		})
+		.then(response => {
+			if (response.ok) {
+				return response.json()
+			}
+		})
+		.then(json => {
+			env.f.write(`<span><cmd onclick="this.parentNode.querySelector('info').removeAttribute('style'); this.remove()" >[show the raw data]</cmd><info style="display: none;" >` + json + `</info></span>`)
+			return
+		})
+		.catch(err => {env.f.write(('<err>' + err + '</err>').toLowerCase())})
+	}
 
 }
 
 
 env.f.write = function(str) {
 	// 显示结果
-	var span = document.createElement('a')
+	var span = document.createElement('span')
 		span.innerHTML = str + '<br />'
 
 	env.e.output.appendChild(span)
+	window.scrollTo(0, document.documentElement.scrollHeight)
 }
 
 
@@ -138,6 +165,7 @@ env.e.input.addEventListener('keypress', function (e) {
 
 
 env.e.output.innerHTML = ''
+env.e.output.setAttribute('style', 'min-height: ' + (window.innerHeight - 40) + 'px')
 env.f.write(new Date())
 env.f.write('init env<br />')
 env.f.write('<info>login to use the sys functions<info>')
