01df2988fcf1fad6d74a2c68ae45a3066a9c7228
Dragoft <16841977@qq.com>
2025-01-18 17:35:58 +0800

    UPDATE

--- functions/remark.api.js
+++ functions/remark.api.js
@@ -7,10 +7,19 @@
 
 	// 删除留言
 	if (m == "0") {
-		await context.env.MetaDB.prepare('DELETE FROM pool WHERE op = 1 and id = ?').bind(body.id).first()
-		await context.env.MetaDB.prepare('UPDATE root set data=data-1 where name="comment"').first()
+		var now = new Date()
+		var options = {timeZone: 'Asia/Shanghai', year: 'numeric', month: '2-digit', day: '2-digit', hour: '2-digit', minute: '2-digit', second: '2-digit', hour12: false }
+		var formatter = new Intl.DateTimeFormat('en-US', options)
+		var parts = formatter.formatToParts(now).reduce((acc, part) => ({ ...acc, [part.type]: part.value }), {})
+		var id = `${parts.year}-${parts.month}-${parts.day} ${parts.hour}:${parts.minute}:${parts.second}`.replace(/:/g, '').replace(/-/g, '').replace(/ /g, '')
 
-		r.msg = {delete: id}
+		if ((parseInt(body.id.replace(/:/g, '').replace(/-/g, '').replace(/ /g, '')) + 7000000) > parseInt(id)) {
+			await context.env.MetaDB.prepare('DELETE FROM pool WHERE op = 0 and id = ?').bind(body.id).first()
+			await context.env.MetaDB.prepare('UPDATE root set data=data-1 where name="comment"').first()
+
+			r.success = true
+			r.msg = {delete: id}
+		}
 	}
 
 	// 添加留言
@@ -27,6 +36,7 @@
 		await context.env.MetaDB.prepare('INSERT INTO pool (id, op, name, content) VALUES (?, ?, ?, ?)').bind(id, '0', body.name, body.content).first()
 		await context.env.MetaDB.prepare('UPDATE root set data=data+1 where name="comment"').first()
 
+		r.success = true
 		r.msg = {add: id}
 	}
 
